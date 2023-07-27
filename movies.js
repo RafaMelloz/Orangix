7673c6f262d474565807d6833aa5bb102228f53d
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2023-07-27 20:53:37 -0300

    teste layout

--- movies.js
+++ movies.js
@@ -11,16 +11,16 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=2786063ea7d6f28dd0543d
                 <div class="tamanho-poster">
                     <div class="posterFilme" style="background-image:url(https://image.tmdb.org/t/p/w500/`+ val.poster_path + `)"></div>
                     <div class="controllers-poster">
-                        <h4 style="text-align:center;font-size: 14px;">`+ val.title + `</h4>
+                        <h4 style="text-align:center;font-size: 20px;" class="font-logo">`+ val.title + `</h4>
                         <div class="flex-arr">
-                        <button class="btn-dark" data-bs-toggle="modal" data-bs-target="#modalFilme`+ val.id + `">Saiba mais</button>
                         <span>` + val.vote_average + `/10 <i class="fa fa-star" aria-hidden="true" style="color:yellow"></i></span>
+                        <button class="btn-poster" data-bs-toggle="modal" data-bs-target="#modalFilme`+ val.id + `">Saiba mais</button>
                         </div>
                     </div>
                 </div>
 
-                <div class="modal fade" id="modalFilme`+ val.id + `" tabindex="-1" aria-labelledby="ModalFilmeLabel" aria-hidden="true">
-                    <div class="modal-dialog">
+                <div class="modal fade center" id="modalFilme`+ val.id + `" tabindex="-1" aria-labelledby="ModalFilmeLabel" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
                         <div class="modal-content dark-modal">
                             <div class="modal-header" style="background-image:url(https://image.tmdb.org/t/p/w500/`+ val.backdrop_path + `)"></div>
                             <div class="modal-body">
@@ -28,7 +28,7 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=2786063ea7d6f28dd0543d
                                 <p>`+ val.overview+`</p>
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn-dark" data-bs-dismiss="modal">Fechar</button>
+                                <button type="button" class="btn-poster" data-bs-dismiss="modal" style="margin:0 auto">Fechar</button>
                             </div>
                         </div>
                     </div>
@@ -38,27 +38,28 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=2786063ea7d6f28dd0543d
 
     //FILTROS
 
-    const filter = document.getElementById('pesquisa')
-    const posters = document.querySelectorAll('.tamanho-poster')
+    const filter = document.getElementById('pesquisa') // Pega o valor do input
+    const posters = document.querySelectorAll('.tamanho-poster') // Pega o o filme
     
     filter.addEventListener('input', filtroFilmes);
 
     function filtroFilmes() {
         if (filter.value != '') {
             for (let poster of posters) {
-                let title = poster.querySelector('.controllers-poster h4')
-                title = title.textContent.toLocaleLowerCase();
-                let filterText = filter.value.toLocaleLowerCase();
 
-                if (!title.includes(filterText)) {
-                    poster.style.display = "none"
+                let title = poster.querySelector('.controllers-poster h4') // pega o titulo do filme
+                title = title.textContent.toLocaleLowerCase();      // passa o titulo para minusculo
+                let filterText = filter.value.toLocaleLowerCase();  // passa o valor do input para minusculo
+
+                if (!title.includes(filterText)) {  // se o valor do titulo não for igual ao do filtro ele some
+                    poster.style.display = "none";
                 }else{
-                    poster.style.display = "block"
+                    poster.style.display = "flex";  // se for ele continua
                 }
             }
         } else {
             for (let poster of posters){
-                poster.style.display ="block"
+                poster.style.display ="flex"
             }
         }
     }
